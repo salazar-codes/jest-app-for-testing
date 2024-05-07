ed06bc85c1ade718700b9ac7dab273af34cea725
Jimmy Salazar <jimmyr.salazar@gmail.com>
2024-05-06 22:26:09 -0500

    test: add getTotalPrice func

--- jest.config.js
+++ jest.config.js
@@ -2,5 +2,5 @@ module.exports = {
   preset: 'jest-preset-angular',
   globalSetup: 'jest-preset-angular/global-setup',
   collectCoverage: true, // se crea la carpeta coverage automáticamente
-  coverageDirectory: 'coverage/jest-example'
+  coverageDirectory: 'coverage/app-for-testing'
 };
--- src/app/pages/cart/cart.component.spec.ts
+++ src/app/pages/cart/cart.component.spec.ts
@@ -4,6 +4,31 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { BookService } from '../../services/book.service';
 import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/compiler';
 import { NO_ERRORS_SCHEMA } from '@angular/core';
+import { Book } from 'src/app/models/book.model';
+
+const listCartBook: Book[] = [
+    {
+        name: '',
+        author: '',
+        isbn: '',
+        price: 15,
+        amount: 2,
+    },
+    {
+        name: '',
+        author: '',
+        isbn: '',
+        price: 20,
+        amount: 1,
+    },
+    {
+        name: '',
+        author: '',
+        isbn: '',
+        price: 8,
+        amount: 7,
+    }
+];
 
 describe('Cart component',()=>{
 
@@ -41,5 +66,25 @@ describe('Cart component',()=>{
         expect(component).toBeTruthy();
         //expect(component).not.toBeTruthy();
     })
+
+    it('getTotalPrice returns an amount',()=>{
+        // Simulando la llamada al mètodo
+        const totalPrice = component.getTotalPrice(listCartBook);
+        // que sea positivo
+        expect(totalPrice).toBeGreaterThan(0);
+        expect(totalPrice).not.toBe(0);
+        expect(totalPrice).not.toBeNull();
+
+    });
+
+    /**
+     * public getTotalPrice(listCartBook: Book[]): number {
+    let totalPrice = 0;
+    listCartBook.forEach((book: Book) => {
+      totalPrice += book.amount * book.price;
+    });
+    return totalPrice;
+  }
+     */
 
 })

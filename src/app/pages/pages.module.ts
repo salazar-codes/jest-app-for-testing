5c7476578f5983cb756d69f34cc7efa25344ed42
Jimmy Salazar <jimmyr.salazar@gmail.com>
2024-05-07 18:31:37 -0500

    test: add form control tests

--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -10,7 +10,8 @@ import { HomeComponent } from './home/home.component';
 import { CartComponent } from './cart/cart.component';
 import { NavModule } from '../nav/nav.module';
 import { ReduceTextPipe } from '../pipes/reduce-text/reduce-text.pipe';
-
+import { FormComponent } from './form/form.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   imports: [
@@ -18,11 +19,14 @@ import { ReduceTextPipe } from '../pipes/reduce-text/reduce-text.pipe';
     CommonModule,
     NavModule,
     PagesRoutingModule,
+    FormsModule,
+    ReactiveFormsModule,
   ],
   declarations: [
     PagesComponent,
     HomeComponent,
     CartComponent,
+    FormComponent,
     ReduceTextPipe,
   ],
   exports: [
